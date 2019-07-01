240751da50131df1f0038a45bf6ad74060f0e53b
haku-noir <hakunoir.00@gmail.com>
2019-07-01 23:32:39 +0900

    chore: 各モジュールのインストールとインポート

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const app = express();
+const cors = require('cors')({origin: true});
+app.use(cors);
