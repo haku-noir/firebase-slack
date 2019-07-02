9de28d35f9a0f0ca0b95f3466373dcaff0337d42
haku-noir <hakunoir.00@gmail.com>
2019-07-03 00:33:53 +0900

    add: チャンネルの一覧を取得するAPIの追加

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors')({origin: true});
 app.use(cors);
+
+app.get('/channels', (req, res) => {
+  let channelsRef = admin.database().ref('channels');
+  channelsRef.once('value', snapshot => {
+    let items = new Array();
+    snapshot.forEach(childSnapshot => {
+        let cname = childSnapshot.key;
+        items.push(cname);
+    });
+
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.send({channels: items});
+  });
+});
