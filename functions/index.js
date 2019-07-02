8815a08e6a69cf166be5d846d085d2fc52f466bd
haku-noir <hakunoir.00@gmail.com>
2019-07-03 00:36:25 +0900

    add: チャンネルを追加するAPIの追加

--- functions/index.js
+++ functions/index.js
@@ -20,3 +20,43 @@ app.get('/channels', (req, res) => {
     res.send({channels: items});
   });
 });
+
+app.post('/channels', (req, res) => {
+  let cname = req.body.cname;
+  createChannel(cname);
+
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.status(201).json({result: 'ok'});
+});
+
+const createChannel = cname => {
+  let channelsRef = admin.database().ref('channels');
+
+  let date1 = new Date();
+  let date2 = new Date();
+  date2.setSeconds(date2.getSeconds() + 1);
+  const defaultData = `{
+      "messages" : {
+          "1" : {
+              "body" : "Welcome to #${cname} channel!",
+              "date" : "${date1.toJSON()}",
+              "user" : {
+                  "avatar" : "",
+                  "id" : "robot",
+                  "name" : "Robot"
+              }
+          },
+          "2" : {
+              "body" : "はじめてのメッセージを投稿してみましょう。",
+              "date" : "${date2.toJSON()}",
+              "user" : {
+                  "avatar" : "",
+                  "id" : "robot",
+                  "name" : "Robot"
+              }
+          }
+      }
+  }`;
+
+  channelsRef.child(cname).set(JSON.parse(defaultData));
+};
